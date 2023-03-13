c209547fae302ad8e5f8a4ffd2fcc670408272a3
dapangmaox <oliver.xinwei.liang@gmail.com>
2023-03-13 23:38:04 +0800

    feat: 添加工程列表

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,38 @@
+import QueryString from 'qs';
+import { useEffect, useState } from 'react';
+import { cleanObject } from '../../utils';
+import { List } from './list';
+import { SearchPanel } from './search-panel';
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: '',
+    personId: '',
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${QueryString.stringify(cleanObject(param))}`).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users} />
+      <List list={list} users={users} />
+    </div>
+  );
+};
