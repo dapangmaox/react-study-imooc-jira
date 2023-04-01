0503c84f2363718ca0b413c78141059e43476f88
dapangmaox <oliver.xinwei.liang@gmail.com>
2023-04-01 12:31:10 +0800

    feat：添加 error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { AuthenticatedApp } from 'authenticated-app';
+import { ErrorBoundry } from 'components/error-boundary';
+import { FullPageErrorFallback } from 'components/lib';
 import { useAuth } from 'context/auth-context';
 import { UnauthenticatedApp } from 'unauthenticated-app';
 import './App.css';
 
 function App() {
   const { user } = useAuth();
-  return <div className="App">{user ? <AuthenticatedApp /> : <UnauthenticatedApp />}</div>;
+  return (
+    <div className="App">
+      <ErrorBoundry fallbackRender={FullPageErrorFallback}>
+        {user ? <AuthenticatedApp /> : <UnauthenticatedApp />}
+      </ErrorBoundry>
+    </div>
+  );
 }
 
 export default App;
